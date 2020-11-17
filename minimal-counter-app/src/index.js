5ed426013d2df29a6ca50803de6360c573b9493a
vvictorious <vvictorious30@gmail.com>
2020-11-17 10:29:43 -0700

    about halfway done with redux boilerplate

--- minimal-counter-app/src/index.js
+++ minimal-counter-app/src/index.js
@@ -2,6 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+
+//store
+
+// actions
+
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// reducer
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state +1
+    case 'DECREMENT':
+      return state - 1
+  }
+}
+
+let store = createStore(counter)
+
+//display it in console
+
+// dispatch
+
+
 
 ReactDOM.render(
   <React.StrictMode>
