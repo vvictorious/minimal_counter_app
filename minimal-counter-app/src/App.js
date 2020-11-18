74f2ac857b594da007ec0eb8ff022c79eac64545
vvictorious <vvictorious30@gmail.com>
2020-11-18 10:22:29 -0700

    this is an interesting way of dispatching actions. I think a little confusing compared to how I was doing it in the it_logger I did, but I guess this works as well

--- minimal-counter-app/src/App.js
+++ minimal-counter-app/src/App.js
@@ -1,12 +1,22 @@
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {increment} from './actions'
 
 function App() {
 
   const counter = useSelector( state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged ? <h3>Valuable info I should only be able to see if I am logged in</h3> : ''
+      }
+        
+
     </div>
   );
 }
